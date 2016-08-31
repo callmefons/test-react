54df2923903746cc8dbb7be7a576b89b446b0eed
Tittaya <callmefons@gmail.com>
2016-08-31 16:25:19 +0700

    first commit

---
+++ ui/components/index.js
@@ -0,0 +1,6 @@
+export App from './App/App'
+
+export Home from './Home'
+
+export Pages from './Pages/Index'
+export ShowPage from './Pages/Show'
---
+++ ui/constants/endpoints.js
@@ -0,0 +1,4 @@
+const API_ROOT = '/api/v1'
+
+// มีค่าเป็น /api/v1/pages
+export const PAGES_ENDPOINT = `${API_ROOT}/pages`
